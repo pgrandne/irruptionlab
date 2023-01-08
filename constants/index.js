5d8043cdf0c7a28c0cd76fd48ab7e41bc97d958d
Perrin <perrin.grandne@protonmail.com>
2023-01-09 00:18:33 +0100

    new version of website

---
+++ constants/index.js
@@ -0,0 +1,84 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Interpool',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'No Pool No Game',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Plz Nft Me',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Tipool',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Exploring Blockchain',
+  },
+];
+
+export const startingFeatures = [
+  'Contact us to bring your idea to life',
+  'We will analyze your needs and propose a quote',
+  'Lets start to work together',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+      'Web3 brings a lot of new features, it is a huge opportunity to correct current issues',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Own your data',
+    subtitle:
+      'The Web3 is based on several principles : transparency, security, ...',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Interpool won several prizes on TRON Hackaton',
+    subtitle:
+      'We have participated to TRON Hackathon during November 2022.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'No Pool No Game won several prize on ETH Online 2022 Hackaton',
+    subtitle:
+      'We have participated to ETH Online 2022 in September 2022 an won 3 prizes',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Irruption Lab will be present at Edcon Vienna 2023 and ETH Porto 2023',
+    subtitle:
+      'In March and April 2023 we will participate to several community events. Happy to meet you here',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+];
---
+++ sections/Hero.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+const Hero = () => (
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10 top-[60px]">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Building applications
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>for Web3 democratization</h1>
+        </motion.div>
+      </div>
+
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[120px] -mt-[100px]"
+      >
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+
+        <img
+          src="/cover.png"
+          alt="hero_cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
+        <img
+          src="/logo_IL.png"
+          alt="hero_cover"
+          className="absolute left-1/2 h-[30%] bottom-[150px] z-10 sepia"
+        />
+
+        <a href="#explore">
+          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+            <img
+              src="/stamp.png"
+              alt="stamp"
+              className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+            />
+          </div>
+        </a>
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default Hero;
