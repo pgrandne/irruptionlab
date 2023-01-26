b4e9fb75b1d3c43a45871ae6e431bd5530b9d75a
Perrin <perrin.grandne@protonmail.com>
2023-01-26 22:56:22 +0100

    improvements for tailwind hero section and navbar

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -6,43 +6,35 @@ import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 import Image from "next/image";
 
 const Hero = () => (
-  <section className={`${styles.yPaddings} mt-0 sm:mt[100] sm:pl-20 pl-10`}>
-
-<motion.div
+  <section className={`${styles.yPaddings} mt-[100px] sm:pl-16 pl-6`}>
+    <motion.div
       variants={staggerContainer()}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-<div className="flex flex-row ">
-  <div className="basis-1/4  ">
-       <motion.div
+      <div className="relative flex flex-col ml-[100px] lg:ml-[350px] mr-[10px] sm:ml-[250px]">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Building applications <br />
+          for Web3 democratization
+        </motion.h1>
+      </div>
+      <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-       
+        className="relative -mt-[100px] sm:-mt-[160px] sm:ml-[80px]"
       >
-        <div className=" gradient-IL-Logo" />
+        <div className="absolute w-[50%] inset-0 gradient-IL-Logo" />
         <Image
-          className="w-[75px] sm:w-[150px] align-middle justify-self-end"
+          className="w-[75px] sm:w-[150px] lg:[250px]"
           src="/logo_IL.svg"
           alt="logo irruption lab"
           width={250}
           height={250}
         />
       </motion.div>
-  </div>
-  <div className="basis-3/4 inline-block align-middle">
-      <div className="">
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          Building applications <br />
-          for Web3 democratization
-        </motion.h1>
-      </div>
-  </div>
-</div>
-</motion.div>
-
-  </section>
+    </motion.div>
+  </section >
 );
 
 export default Hero;
